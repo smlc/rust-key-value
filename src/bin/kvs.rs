b30d376374544f4fbcd39e7437447bed01f0e6d9
Marlon Siesa <marlon.siesa@moo.com>
2021-12-06 21:58:51 +0000

    Part 2: Accept command line arguments done

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,4 +1,43 @@
+use clap::{App, Arg, SubCommand, AppSettings};
+use kvs::KvStore;
+
+
 fn main() {
-	println!("Hello, world!");
+
+    let matches = App::new("kvs")
+        .version("0.1.0")
+        .author("Marlon S. <sm@mail.com>")
+        .about("Key value store")
+        .setting(AppSettings::SubcommandRequired)
+        .subcommand(
+            SubCommand::with_name("set")
+                .about("Sets key value in the store")
+                .arg(Arg::with_name("key").required(true))
+                .arg(Arg::with_name("value").required(true)),
+        )
+        .subcommand(
+            SubCommand::with_name("get")
+                .about("Get value for the given key")
+                .arg(Arg::with_name("key").required(true))
+        )
+        .subcommand(
+            SubCommand::with_name("rm")
+                .about("Delete entry for the given key")
+                .arg(Arg::with_name("key").required(true))
+        )
+    .get_matches();
+    
+    let mut kvs = KvStore::new();
+    if let Some(matches) = matches.subcommand_matches("set") {
+        kvs.set(matches.value_of("key").unwrap().to_string(), matches.value_of("value").unwrap().to_string());
+    }
+
+    if let Some(matches) = matches.subcommand_matches("get") {
+        kvs.get(matches.value_of("key").unwrap().to_string());
+    }
+
+    if let Some(matches) = matches.subcommand_matches("rm") {
+        kvs.remove(matches.value_of("key").unwrap().to_string());
+    }
 }
     
---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+#[warn(unused_variables)]
+
+pub struct KvStore {
+}
+
+impl KvStore {
+    pub fn new() -> KvStore {
+        KvStore{}
+    }
+    pub fn set(&mut self, _key: String, _value: String) {
+        panic!("unimplemented")
+    }
+    
+    pub fn get(&self, _key: String) -> Option<String> {
+        panic!("unimplemented")
+    }
+
+    pub fn remove(&mut self, _key: String){
+        panic!("unimplemented")
+    }
+}
